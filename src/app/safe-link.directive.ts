4ad5d3c9596a6206da021dc60b8f42d04dbfb069
Mehvish Fansopkar <mehvishfansopkar8@gmail.com>
2024-10-22 16:12:33 +0530

    155. Directives & Dependency Injection

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,26 +1,28 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
-    selector: 'a[appSafeLink]',
-    standalone: true,
-    host: {
-        '(click)': 'onConfirmLeavePage($event)'
-    }
-
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
-export class SafeLinkDirective{
-    queryParam = input('my-app', {alias: 'appSafeLink'})
-    constructor(){
-        console.log("SafeLink directive is active");
-    }
+export class SafeLinkDirective {
+  queryParam = input('my-app', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
-    onConfirmLeavePage(event: MouseEvent){
-        const wantsToLeave = window.confirm("Do you want to leave the app?");
-        if(wantsToLeave){
-            const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + this.queryParam();
-            return;
-        }
-        event.preventDefault();
+  constructor() {
+    console.log('SafeLink directive is active');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+    if (wantsToLeave) {
+      const address =this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href = address + this.queryParam();
+    //   (event.target as HTMLAnchorElement).href = address + this.queryParam();
+      return;
     }
+    event.preventDefault();
+  }
 }
